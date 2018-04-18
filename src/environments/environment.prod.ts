8237fc5655dd403c987951c7a4e62a85e9afc35b
Don Henton <donhenton@gmail.com>
2018-04-18 10:51:46 -0500

    setting up for prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
+  securityAPIURL: 'http://donhenton-spring-boot.herokuapp.com/sec/',
   swaggerUrl: 'http://donhenton-spring-boot.herokuapp.com/swagger-ui.html'
 };
