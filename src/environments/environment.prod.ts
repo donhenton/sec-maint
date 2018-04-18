7507335f2955a270aa2a4ba981169921fe9cd142
Don Henton <dhenton@amfam.com>
2018-04-18 13:58:30 -0500

    setting gh-pages

--- src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
-  securityAPIURL: 'https://donhenton-spring-boot.herokuapp.com/sec/',
-  swaggerUrl: 'https://donhenton-spring-boot.herokuapp.com/swagger-ui.html'
-};
