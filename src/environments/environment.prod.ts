ecfb02c7990d37ef1092f795d1e3a017a6519fd5
Don Henton <donhenton@gmail.com>
2019-12-23 10:30:48 -0600

    pointing to aws

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,13 @@
+/*
 export const environment = {
   production: true,
   securityAPIURL: 'https://donhenton-spring-boot.herokuapp.com/sec/',
   swaggerUrl: 'https://donhenton-spring-boot.herokuapp.com/swagger-ui.html'
+};
+*/
+
+export const environment = {
+  production: true,
+  securityAPIURL: 'http://birt.awsdhenton.com:9000/sec/',
+  swaggerUrl: 'http://birt.awsdhenton.com:9000/swagger-ui.html'
 };
